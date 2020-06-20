2ceccb6fd23f719efeb508adc774b71046720165
DuoLa <hari@gitlab.com>
2020-06-20 15:40:16 +0800

    完善测试 demo

--- Example/M3u8DownLoadManger/ViewController.swift
+++ Example/M3u8DownLoadManger/ViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 import M3u8DownLoadManger
 
+import ZFPlayer
+
 class ViewController: UIViewController {
     
-//    M3u8DownLoad.manger.downLoadProTs("http://vcache.city84.com/video/c003596/audio/4d2fbe6229df4795accbae3bdc32225c.m3u8")
-
+    lazy var player: ZFPlayerController = {
+        var playerss = ZFPlayerController.init(playerManager: ZFAVPlayerManager.init(), containerView: view)
+        playerss.assetURL = M3u8Cache.proxyLocal(url: "http://vcache.city84.com/video/c003596/audio/4d2fbe6229df4795accbae3bdc32225c.m3u8")
+        return playerss
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
+        M3u8DownLoad.manger.downLoadProTs(url: "http://vcache.city84.com/video/c003596/audio/4d2fbe6229df4795accbae3bdc32225c.m3u8")
+        
         // Do any additional setup after loading the view, typically from a nib.
+        player.playTheIndex(0)
     }
 
     override func didReceiveMemoryWarning() {
